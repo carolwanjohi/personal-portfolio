0840ed01189e278f54b5908bc78901d35fb62463
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-16 14:16:19 +0300

    Create Creator and Project models Create Creator class for the creators table in the database Create test case for the instance of Creator class Create test case for the save_creator method in Creator class Create test case for the delete_creator method in the Creator class Create test case for the class method get creators in the Creator class Create migration to database with Creator class, 0001_initial.py
    Create Project class for the projects table in the database
    Create test case for the instance of Project class
    Create test case for the save_project method in Project class
    Create test case for the delete_project method in the Project class
    Create test case for the class method get projects in the Project class
    Create migration to database with Project class, 0002_project.py

--- portfolio_website/portfolio/models.py
+++ portfolio_website/portfolio/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+class Creator(models.Model):
+    '''
+    Class that defines a Creator for a project
+    '''
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+    def save_creator(self):
+        '''
+        Method to save a new Creator to the database
+        '''
+        self.save()
+
+    def delete_creator(self):
+        '''
+        Method to delete a Creator from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_creators(cls):
+        '''
+        Method that gets all creators from the database
+
+        Returns:
+            creators : list of creator objects from the database
+        '''
+        creators = Creator.objects.all()
+        return creators
+
+class Project(models.Model):
+    '''
+    Class that defines a Project
+    '''
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    git_hub_link = models.CharField(max_length=255)
+    deployed_link = models.CharField(max_length=255)
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_project(self):
+        '''
+        Method to save a new Project to the database
+        '''
+        self.save()
+
+    def delete_project(self):
+        '''
+        Method to delete a Project from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_projects(cls):
+        '''
+        Method that gets all projects from the database
+
+        Returns:
+            projects : list of project objects from the database
+        '''
+        projects = Project.objects.all()
+        return projects
+
+
--- portfolio_website/portfolio/tests.py
+++ portfolio_website/portfolio/tests.py
@@ -1,3 +1,101 @@
 from django.test import TestCase
+from .models import Creator, Project
 
 # Create your tests here.
+class CreatorTestClass(TestCase):
+    '''
+    Test case for Creator class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up a Creator instance before each test
+        '''
+        # Create a creator instance
+        self.new_creator = Creator(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_creator in an instance of Creator class
+        '''
+        self.assertTrue( isinstance(self.new_creator, Creator) )
+
+    def test_save_creator(self):
+        '''
+        Test case to check is a creator is saved in the database
+        '''
+        self.new_creator.save_creator()
+        creators = Creator.objects.all()
+        self.assertTrue( len(creators) > 0 )
+
+    def test_delete_creator(self):
+        '''
+        Test case to check if a creator is deleted from the database
+        '''
+        self.new_creator.save_creator()
+        creators = Creator.objects.all()
+        self.new_creator.delete_creator()
+        self.assertTrue( len(creators) == 0)
+
+    def test_get_creators(self):
+        '''
+        Test case to check if all creators are gotten from the database
+        '''
+        self.new_creator.save_creator()
+        gotten_creators = Creator.get_creators()
+        creators = Creator.objects.all()
+        self.assertTrue( len(gotten_creators) == len(creators))
+
+
+class ProjectTestClass(TestCase):
+    '''
+    Test case for Project class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up a Project instance before each test
+        '''
+        # Create and save a creator instance
+        self.james = Creator(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_creator()
+
+        # Create a project instance
+        self.new_project = Project(name = 'Python James', description ='Python James is Muriuki who wrote Python content for Moringa School', git_hub_link='https://github.com/tester/test-link', deployed_link='https://test/deployed-link', creator=self.james)
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_project in an instance of Project class
+        '''
+        self.assertTrue( isinstance(self.new_project, Project) )
+
+    def test_save_project(self):
+        '''
+        Test case to check is a Project is saved in the database
+        '''
+        self.james.save_creator()
+        self.new_project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue( len(projects) > 0 )
+
+    def test_delete_project(self):
+        '''
+        Test case to check if an project is deleted from the database
+        '''
+        self.new_project.save_project()
+        projects = Project.objects.all()
+        self.new_project.delete_project()
+        self.assertTrue( len(projects) == 0 )
+
+    def test_get_projects(self):
+        '''
+        Test case to check if all projects are gotten from the database
+        '''
+        self.new_project.save_project()
+        gotten_projects = Project.get_projects()
+        projects = Project.objects.all()
+        self.assertTrue( len(gotten_projects) == len(projects))
+
+
