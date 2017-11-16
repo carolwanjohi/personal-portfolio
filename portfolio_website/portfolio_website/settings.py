772486536459656e5da1ac1914c45aacb59c13ea
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-16 11:43:31 +0300

    Set up index page Instal bootstrap3 and update requirements.txt Regirster portfolio app and bootsratp3 Update urls to include portfolio index route as site route

--- portfolio_website/portfolio_website/settings.py
+++ portfolio_website/portfolio_website/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
